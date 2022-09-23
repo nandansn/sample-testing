f05372a5e8e23e69e89be886a4f20635105b1728
nrangasa <nandakumar.rangasamy@oracle.com>
2022-09-23 00:11:10 -0700

    updated test

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 let btn = document.querySelector("#btn");
 let result = document.querySelector("span#result");
+let cart = document.querySelector("span#cart-message");
+
+let select = document.querySelector("#category");
+
+
+let plusBtn = document.querySelector("#plus");
+let minusBtn = document.querySelector("#minus");
+
+let cartCount = 0
+
+select.addEventListener("change", () => {
+    
+  cartCount = 0;
+  let selectedValue = document.querySelector("#category").value
+
+  if (selectedValue !== 'none') {
+    result.textContent = `User selected the category : ${selectedValue}`;
+  }
+  
+  document.querySelector("#item-count").textContent = cartCount
+});
+
+
 
 btn.addEventListener("click", () => {
-    result.textContent = `User selected the category : ${document.querySelector("#category").value}`;
+    cartCount = document.querySelector("#item-count").textContent;
+    cart.textContent = `Total ${
+      document.querySelector("#category").value
+    } added to the cart: ${cartCount}`;
 })
+
+
+plusBtn.addEventListener("click", () => {
+  ++cartCount;
+  document.querySelector("#item-count").textContent = cartCount;
+});
+
+
+minusBtn.addEventListener("click", () => {
+  --cartCount;
+  document.querySelector("#item-count").textContent = cartCount;
+});
+
+
