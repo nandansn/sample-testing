221b235020c7bc1e21b6e7c870483e8842eb0859
nrangasa <nandakumar.rangasamy@oracle.com>
2022-09-24 02:23:20 -0700

    updated

---
+++ data.js
@@ -0,0 +1,26 @@
+export let products = [
+  {
+    type: "Books",
+    img_src:
+      "https://images-na.ssl-images-amazon.com/images/I/51YP7fM361S._SX460_BO1,204,203,200_.jpg",
+    title: "The Time Machine",
+    available_quantity: 10,
+    price: "Rs 100",
+  },
+
+  {
+    type: "Mobile",
+    img_src: "https://m.media-amazon.com/images/I/61YSNhAb00L._SL1500_.jpg",
+    title: "iPhone 14 Plus 256GB Purple",
+    available_quantity: 20,
+    price: "Rs 99,900",
+  },
+
+  {
+    type: "Computer",
+    img_src: "https://m.media-amazon.com/images/I/61bX2AoGj2L._SX679_.jpg",
+    title: "MacBook Pro Laptop",
+    available_quantity: 4,
+    price: "Rs 1,49,900.",
+  },
+];
--- index.js
+++ index.js
@@ -1,46 +1,101 @@
-let btn = document.querySelector("#btn");
+import { products } from './data.js'
+
+
+
 let result = document.querySelector("span#result");
-let cart = document.querySelector("span#cart-message");
-
-let select = document.querySelector("#category");
+let selectCategory = document.querySelector("#category");
 
 
-let plusBtn = document.querySelector("#plus");
-let minusBtn = document.querySelector("#minus");
+let plusBtn
+let minusBtn 
 
-let cartCount = 0
 
-select.addEventListener("change", () => {
-    
+
+let cartMessage
+
+let cartCount = 0;
+
+// category selection
+selectCategory.addEventListener("change", (e) => {
+    e.preventDefault()
   cartCount = 0;
-  let selectedValue = document.querySelector("#category").value
+  let selectedValue = e.target.value;
+  let cartContaner = document.getElementById("cart-container");
+  cartContaner.style.display = "none";
 
-  if (selectedValue !== 'none') {
-    result.textContent = `User selected the category : ${selectedValue}`;
-  }
   
-  document.querySelector("#item-count").textContent = cartCount
+    if (selectedValue !== "none") {
+      result.textContent = `User selected the category : ${selectedValue}`;
+      createProductContainer(selectedValue);
+    }
+
+  document.querySelector("#item-count").textContent = cartCount;
+  console.log(cartCount);
+
+
 });
 
+function createProductContainer(name) {
+    
+    let item = {...products.find(product => product.type === name)};
+
+    let prodContainer = document.getElementById("product");
+
+    prodContainer.style.visibility = "visible"
+    prodContainer.querySelector("img").src = item.img_src
+    prodContainer.querySelector("#title").textContent = item.title
+    prodContainer.querySelector("#price").textContent = item.price
+    prodContainer.querySelector("#quantity").textContent = item.available_quantity;
+
+    
+
+        
+}
 
 
-btn.addEventListener("click", () => {
-    cartCount = document.querySelector("#item-count").textContent;
-    cart.textContent = `Total ${
-      document.querySelector("#category").value
-    } added to the cart: ${cartCount}`;
-})
+let counter = document.getElementById("quantity-counter");
 
+counter.style.display = "block";
+document.getElementById("item-count").style.display = "block";
+
+plusBtn = document.querySelector("#plus");
+minusBtn = document.querySelector("#minus");
+cartMessage = document.querySelector("span#cart-message");
 
 plusBtn.addEventListener("click", () => {
+  cartCount = parseInt(document.querySelector("#item-count").textContent);
   ++cartCount;
   document.querySelector("#item-count").textContent = cartCount;
 });
 
-
 minusBtn.addEventListener("click", () => {
+  cartCount = parseInt(document.querySelector("#item-count").textContent);
   --cartCount;
   document.querySelector("#item-count").textContent = cartCount;
 });
+
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  cartCount = document.querySelector("#item-count").textContent;
+  let cartContaner = document.getElementById("cart-container");
+  cartContaner.style.display = "block";
+  cartMessage.textContent = `Total ${
+    document.querySelector("#category").value
+  } added to the cart: ${cartCount}`;
+});
+
+
+// quantity update
+
+
+
+
+
+
+// add product to cart
+
+
+
 
 
